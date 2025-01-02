a760d8622a90a60ee1429eec433b0ad53c729927
Alexandre <alexandrenas2003@gmail.com>
2025-01-02 12:12:50 -0300

    products paged

--- src/main/java/com/example/studycommerce/controllers/ProductController.java
+++ src/main/java/com/example/studycommerce/controllers/ProductController.java
@@ -1,15 +1,13 @@
 package com.example.studycommerce.controllers;
 
 import com.example.studycommerce.DTO.ProductDTO;
-import com.example.studycommerce.entities.Product;
-import com.example.studycommerce.repositories.ProductRepository;
 import com.example.studycommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -17,6 +15,12 @@ public class ProductController {
 
     @Autowired
     private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> products = service.findAll(pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
--- src/main/java/com/example/studycommerce/services/ProductService.java
+++ src/main/java/com/example/studycommerce/services/ProductService.java
@@ -4,10 +4,10 @@ import com.example.studycommerce.DTO.ProductDTO;
 import com.example.studycommerce.entities.Product;
 import com.example.studycommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class ProductService {
@@ -21,5 +21,11 @@ public class ProductService {
             Product product = repository.findById(id).get();
             return new ProductDTO(product);
         } else throw new IllegalArgumentException("Id does not exist.");
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> products = repository.findAll(pageable);
+        return products.map(ProductDTO::new);
     }
 }
